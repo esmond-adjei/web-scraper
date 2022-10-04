af2fc40bfb3b8938ae7360c1fe296b837e7743a2
esmond-adjei <81225469+esmond-adjei@users.noreply.github.com>
2022-10-04 03:52:06 +0000

    modified:   src/webscraper-back/base/views.py

--- src/webscraper-back/base/views.py
+++ src/webscraper-back/base/views.py
@@ -23,7 +23,6 @@ def progress(request):
         download_img = ''
 
     address, movieKeyword = getAddress(movie_type, query)
-    # movieLinksFound, downloadLinks
     scraped_data = recursiveScrape(find_tag('a',scrape(address)), movieKeyword.lower())    
     image_option = download_img
 
@@ -38,31 +37,10 @@ def progress(request):
             IMDB(imgKeyword,True,0.7)
         except:
             print("COULD NOT OBTAIN IMAGE FILE")
-    imgTag = f'''style="background-image: url('./images/{imgKeyword.replace(' ', '_')}.jpg');"'''
+        
     print("========== data ============\n")
-    print(type(scraped_data))
     for k,v in scraped_data.items():
         print('>>',k,'\n\t',v)
     
     return render(request, 'progress.html', {'scraped_data': scraped_data})
-
-#     html_title = f'''<!DOCTYPE html>\n<html lang="en">
-# <head>\n<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">
-# <meta name="viewport" content="width=device-width, initial-scale=1.0">
-# <title>{movieKeyword.capitalize()}</title>\n<link rel="stylesheet" href="../styles.css">
-# </head>\n<body>\n<section class="movieLinks" style="background-image: url('../images/{movieKeyword.replace(' ', '_')}.jpg');">'''
-    
-#     if len(movieLinksFound) > len(movieKeyword)+15:
-#         # saveAs(movieLinksFound,movieKeyword+"(recursive)",'.txt','w')                  # saves ONCE as TXT file
-        
-#         ends_with = "</ul>\n</section>\n</body>\n</html>"
-#         saveAs(html_title,'movie_website/html/'+movieKeyword,'.html','w')       # writing down the html boiler plate first with 'w' to overrite text in file if pressent
-#         saveAs(downloadLinks+ends_with,'movie_website/html/'+movieKeyword,'.html')    # saves ONCE as HTML file
-        
-#         indexHTML_code = f'''\n<a href="./html/{'_'.join(movieKeyword.split())}.html"><div class='box_nav' {imgTag}><h1>{movieKeyword.capitalize()}</h1></div></a>'''
-#         saveAs(indexHTML_code,'movie_website/index','.html')                    # main navigation to connect to DOWNLOAD LINK HTML
-
-#         directory = "file://c:/users/esmon/desktop/scrapes/movie_website/index.html"
-#         print("**__SUCCESS__**\nOpen html link: ", directory)
-#     else:   print("!!__MOVIE LINK SEARCH NOT SUCCESSFUL__!!")
 
