629b903d33781036fd5e1fee01a25076578b7fdb
esmond-adjei <81225469+esmond-adjei@users.noreply.github.com>
2022-10-10 07:41:29 +0000

    modified:   src/webscraper-back/base/__pycache__/admin.cpython-310.pyc 	modified:   src/webscraper-back/base/__pycache__/models.cpython-310.pyc 	modified:   src/webscraper-back/base/admin.py 	new file:   src/webscraper-back/base/migrations/0002_alter_moviedata_movielink.py 	modified:   src/webscraper-back/base/models.py 	modified:   src/webscraper-back/db.sqlite3 	new file:   src/webscraper-back/images/mulan.jpg

--- src/webscraper-back/base/models.py
+++ src/webscraper-back/base/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.forms import CharField, ImageField
+# from django.forms import CharField, ImageField
 
 # Create your models here.
 class MovieData(models.Model):
     query = models.CharField(max_length=100)
     movie = models.CharField(max_length=256)
-    movielink = models.CharField(max_length=2000)
+    movielink = models.CharField(max_length=2000, null=True, blank=True)
     imagelink = models.ImageField(upload_to='images', blank=True)
