1d0403333fdde7bd69091ebed2c37c34c6088549
esmond-adjei <xmongram@gmail.com>
2022-12-04 18:04:20 +0000

    modified:   webscraper-back/base/admin.py 	modified:   webscraper-back/base/models.py

--- webscraper-back/base/models.py
+++ webscraper-back/base/models.py
@@ -2,7 +2,14 @@ from django.db import models
 # from django.forms import CharField, ImageField
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+
+
 class MovieData(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     query = models.CharField(max_length=100)
     movie = models.CharField(max_length=256)
     movielink = models.CharField(max_length=2000, null=True, blank=True)
