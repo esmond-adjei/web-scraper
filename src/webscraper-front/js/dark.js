d5547e1f524f0236d6d0cb9eb01b2a21e65c2c45
Esmond Adjei <xmongram@gmail.com>
2023-01-19 14:31:57 +0000

    removed webscraper-front

--- src/webscraper-front/js/dark.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('change', ()=>{
-    darkTargets = document.querySelectorAll('#turn-dark');
-    // console.log(darkTargets);
-    for (let i = 0; i<darkTargets.length; i++){
-        darkTargets[i].classList.toggle('dark');
-    }
-})
